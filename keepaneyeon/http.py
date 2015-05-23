bec734de59b019a6a78fae61c3a7aa96fe1b8b5f
Michael McLoughlin <mmcloughlin@gmail.com>
2015-05-23 15:21:27 -0700

    HTTP Downloader

---
+++ keepaneyeon/http.py
@@ -0,0 +1,19 @@
+import requests
+
+class HttpDownloader():
+    def __init__(self, opts={}):
+        self.base = opts
+
+    def build_request_options(self, opts):
+        options = {'method': 'get'}
+        options.update(self.base)
+        options.update(opts)
+        options.update({'stream': True})
+        return options
+
+    def download(self, opts, out):
+        options = self.build_request_options(opts)
+        r = requests.request(**options)
+        r.raise_for_status()
+        for chunk in r.iter_content(1024):
+            out.write(chunk)
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(name='keepaneyeon',
+      version='0.1.0',
+      packages=find_packages(),
+      )
