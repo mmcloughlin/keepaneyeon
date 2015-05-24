64ca6a90d87f309c6c07a6192d0ee5b9b977e66f
Michael McLoughlin <mmcloughlin@gmail.com>
2015-05-23 18:34:47 -0700

    exists function for S3 storage

--- keepaneyeon/s3.py
+++ keepaneyeon/s3.py
@@ -24,10 +24,23 @@ class S3Storage():
                 'key': components.path,
                 }
 
+    def key_for_name(self, name):
+        """
+        Return full key path for the named file.
+        """
+        return self.base + '/' + name
+
+    def exists(self, name):
+        """
+        Determine whether the named file exists.
+        """
+        k = self.bucket.get_key(self.key_for_name(name))
+        return bool(k)
+
     def store_from_filename(self, name, filename):
         """
         Store given filename under the provided name.
         """
         k = boto.s3.key.Key(self.bucket)
-        k.key = self.base + '/' + name
+        k.key = self.key_for_name(name)
         k.set_contents_from_filename(filename)
