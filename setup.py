f48dd577b59963112093710ebe225a28325437e5
Michael McLoughlin <mmcloughlin@gmail.com>
2015-05-24 22:22:22 -0700

    More package information

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup, find_packages
 
 setup(name='keepaneyeon',
       version='0.1.0',
+      description='Monitor URLs for changes',
+      url='https://github.com/mmcloughlin/keepaneyeon',
+      author='Michael McLoughlin',
+      license='MIT',
       packages=find_packages(),
       entry_points={
           'console_scripts': [
