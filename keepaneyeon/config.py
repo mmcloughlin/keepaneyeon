41df4741a7e7fc51618d8a929475d4892a0901df
Michael McLoughlin <mmcloughlin@gmail.com>
2015-05-25 14:14:01 -0700

    Tests for configuration

--- keepaneyeon/config.py
+++ keepaneyeon/config.py
@@ -6,19 +6,20 @@ from keepaneyeon.s3 import S3Storage
 from keepaneyeon.versioned import VersionedStorage
 from keepaneyeon.target import Target
 
-class YamlLoader(yaml.Loader):
-    @classmethod
-    def register(self, tag, cls):
-        def constructor(loader, node):
-            opts = loader.construct_mapping(node)
-            return cls(**opts)
-        self.add_constructor('!' + tag, constructor)
+def register(Loader, tag, cls):
+    def constructor(loader, node):
+        opts = loader.construct_mapping(node)
+        return cls(**opts)
+    Loader.add_constructor('!' + tag, constructor)
 
-YamlLoader.register('downloader/http', HttpDownloader)
-YamlLoader.register('downloader/42matters', FortyTwoMattersLookup)
-YamlLoader.register('storage/s3', S3Storage)
-YamlLoader.register('storage/versioned', VersionedStorage)
-YamlLoader.register('target', Target)
+class YamlLoader(yaml.Loader):
+    pass
+
+register(YamlLoader, 'downloader/http', HttpDownloader)
+register(YamlLoader, 'downloader/42matters', FortyTwoMattersLookup)
+register(YamlLoader, 'storage/s3', S3Storage)
+register(YamlLoader, 'storage/versioned', VersionedStorage)
+register(YamlLoader, 'target', Target)
 
 def load(stream):
     return yaml.load(stream, Loader=YamlLoader)
---
+++ tests/test_config.py
@@ -0,0 +1,42 @@
+import unittest
+import yaml
+
+import keepaneyeon.config
+from keepaneyeon.http import HttpDownloader
+
+class TestConfig(unittest.TestCase):
+    def test_register(self):
+        # custom type we want to load from YAML
+        class A():
+            def __init__(self, **opts):
+                self.opts = opts
+        # YAML loader we will customize
+        class CustomLoader(yaml.Loader):
+            pass
+        # register our new type
+        keepaneyeon.config.register(CustomLoader, 'a', A)
+        # parse some YAML
+        config_sting = """
+        - !a
+          k1: v1
+          k2: v2
+        """
+        parsed = yaml.load(config_string, Loader=CustomLoader)
+        self.assertEqual(len(parsed), 1)
+        self.assertIsInstance(parsed[0], A)
+        self.assertEqual(parsed[0].opts['k1'], 'v1')
+        self.assertEqual(parsed[0].opts['k2'], 'v2')
+
+    def test_load(self):
+        # test loading one of our registered types
+        config_string = """
+        - !downloader/http
+          k1: v1
+          k2: v2
+        """
+        parsed = keepaneyeon.config.load(config_string)
+        self.assertEqual(len(parsed), 1)
+        self.assertIsInstance(parsed[0], HttpDownloader)
+        self.assertEqual(parsed[0].base['k1'], 'v1')
+        self.assertEqual(parsed[0].base['k2'], 'v2')
+
