1ab16d40c44d1d486cf401703f534a371c022b4c
Michael McLoughlin <mmcloughlin@gmail.com>
2015-05-30 15:20:16 -0700

    Adding long_description in setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ import re
 pkg_init_file = open('keepaneyeon/__init__.py').read()
 metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", pkg_init_file))
 
+def readme():
+    return open('./README.rst').read()
+
 setup(name='keepaneyeon',
       version=metadata['version'],
       description='Monitor URLs for changes',
+      long_desription=readme(),
       url='https://github.com/mmcloughlin/keepaneyeon',
       author='Michael McLoughlin',
       license='MIT',
